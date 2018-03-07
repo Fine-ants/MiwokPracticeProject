1d48555a1e83f0cc9bffb4250ca3246237ea912c
Jordan H <jordanhuusy@live.com>
2018-03-06 19:57:29 -0800

    Added second constructor for Word object to include drawable integer.

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -7,6 +7,7 @@ package com.example.android.miwok;
 public class Word {
     private String englishWord;
     private String miwokTranslation;
+    private int imageResourceId;
 
 
     public Word(String englishWord, String miwokTranslation) {
@@ -14,6 +15,11 @@ public class Word {
         this.miwokTranslation = miwokTranslation;
     }
 
+    public Word(String englishWord, String miwokTranslation, int imageResourceId) {
+        this.englishWord = englishWord;
+        this.miwokTranslation = miwokTranslation;
+        this.imageResourceId = imageResourceId;
+    }
 
     @Override
     public String toString() {
@@ -29,5 +35,9 @@ public class Word {
 
     public String getMiwokTranslation() {
         return miwokTranslation;
+    }
+
+    public int getImageResourceId() {
+        return imageResourceId;
     }
 }
