3f253470edc0af3efbfae17cd936670e30153469
Jordan H <jordanhuusy@live.com>
2018-03-18 17:49:46 -0700

    Migrated NumbersActivity logic to NumbersFragment.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,124 +15,33 @@
  */
 package com.example.android.miwok;
 
-import android.media.AudioManager;
-import android.media.MediaPlayer;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
-
-    private AudioManager audioManager;
-    private AudioManager.OnAudioFocusChangeListener afChangeListener = new AudioManager.OnAudioFocusChangeListener() {
-        @Override
-        public void onAudioFocusChange(int i) {
-            switch (i){
-                // AUDIOFOCUS_GAIN
-                case AudioManager.AUDIOFOCUS_GAIN:
-                    mediaPlayer.start();
-                    break;
-
-                // AUDIOFOCUS_LOSS
-                case AudioManager.AUDIOFOCUS_LOSS:
-                    mediaPlayer.stop();
-                    break;
-
-                // AUDIOFOCUS_LOSS_TRANSIENT and AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK
-                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
-                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
-                    mediaPlayer.pause();
-                    break;
-            }
-        }
-    };
-    private MediaPlayer mediaPlayer;
-    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
-        @Override
-        public void onCompletion(MediaPlayer mediaPlayer) {
-            releaseMedia();
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-
-        // Retrieve Audio Manager object
-        audioManager = getSystemService(AudioManager.class);
+        setContentView(R.layout.activity_category);
 
 
-        final ArrayList<Word> numbers = new ArrayList<>();
-        numbers.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
-        numbers.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
-        numbers.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
-        numbers.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
-        numbers.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
-        numbers.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
-        numbers.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
-        numbers.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
-        numbers.add(new Word("nine", "wo’e", R.drawable.number_nine, R.raw.number_nine));
-        numbers.add(new Word("ten", "na’aacha", R.drawable.number_ten, R.raw.number_ten));
-
-
-        // Instantiate custom word adapter
-        WordAdapter itemsAdapter = new WordAdapter(this, numbers, R.color.category_numbers);
-
-
-        // Set the adapter to the listView
-        ListView listView = findViewById(R.id.list);
-        listView.setAdapter(itemsAdapter);
-
-        // Set OnItemClickListener for the pronunciation
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                // Request Audio Focus
-                int result = audioManager.requestAudioFocus(afChangeListener, AudioManager.STREAM_NOTIFICATION, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
-                if(result == AudioManager.AUDIOFOCUS_GAIN) {
-                    // Return the color audio resource id
-                    int audio = numbers.get(i).getAudioResourceId();
-
-                    // Prior to starting media player, release if already exists
-                    releaseMedia();
-
-                    // Create a new media player and start
-                    mediaPlayer = MediaPlayer.create(NumbersActivity.this, audio);
-                    mediaPlayer.start();
-
-                    // When media track is finished call releaseMedia method
-                    mediaPlayer.setOnCompletionListener(onCompletionListener);
-                }
-            }
-        });
-
+        // Open the NumbersFragment fragment
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.container, new NumbersFragment());
+        transaction.commit();   
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        releaseMedia();
-    }
-
-    void releaseMedia(){
-        if(mediaPlayer!=null){
-
-            // Release media player object
-            mediaPlayer.release();
-            mediaPlayer=null;
-
-
-            // Media player is complete, abandon audio focus
-            audioManager.abandonAudioFocus(afChangeListener);
-        }
     }
 }
 
--- app/src/main/java/com/example/android/miwok/NumbersFragment.java
+++ app/src/main/java/com/example/android/miwok/NumbersFragment.java
@@ -1,21 +1,128 @@
 package com.example.android.miwok;
 
 import android.app.Fragment;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 /**
  * Created by jordanhuus on 3/18/2018.
  */
 
 public class NumbersFragment extends Fragment {
+
+    private AudioManager audioManager;
+    private AudioManager.OnAudioFocusChangeListener afChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int i) {
+            switch (i){
+                // AUDIOFOCUS_GAIN
+                case AudioManager.AUDIOFOCUS_GAIN:
+                    mediaPlayer.start();
+                    break;
+
+                // AUDIOFOCUS_LOSS
+                case AudioManager.AUDIOFOCUS_LOSS:
+                    mediaPlayer.stop();
+                    break;
+
+                // AUDIOFOCUS_LOSS_TRANSIENT and AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK
+                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                    mediaPlayer.pause();
+                    break;
+            }
+        }
+    };
+    private MediaPlayer mediaPlayer;
+    private MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMedia();
+        }
+    };
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View numberView = inflater.inflate(R.layout.fragment_numbers, container, false);
-        return numberView;
+        View rootView = inflater.inflate(R.layout.word_list, container, false);
+
+        // Retrieve Audio Manager object
+        audioManager = getActivity().getSystemService(AudioManager.class);
+
+
+        final ArrayList<Word> numbers = new ArrayList<>();
+        numbers.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
+        numbers.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
+        numbers.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
+        numbers.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
+        numbers.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
+        numbers.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
+        numbers.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
+        numbers.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
+        numbers.add(new Word("nine", "wo’e", R.drawable.number_nine, R.raw.number_nine));
+        numbers.add(new Word("ten", "na’aacha", R.drawable.number_ten, R.raw.number_ten));
+
+
+        // Instantiate custom word adapter
+        WordAdapter itemsAdapter = new WordAdapter(getActivity(), numbers, R.color.category_numbers);
+
+
+        // Set the adapter to the listView
+        ListView listView = rootView.findViewById(R.id.list);
+        listView.setAdapter(itemsAdapter);
+
+        // Set OnItemClickListener for the pronunciation
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                // Request Audio Focus
+                int result = audioManager.requestAudioFocus(afChangeListener, AudioManager.STREAM_NOTIFICATION, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+                if(result == AudioManager.AUDIOFOCUS_GAIN) {
+                    // Return the color audio resource id
+                    int audio = numbers.get(i).getAudioResourceId();
+
+                    // Prior to starting media player, release if already exists
+                    releaseMedia();
+
+                    // Create a new media player and start
+                    mediaPlayer = MediaPlayer.create(getActivity(), audio);
+                    mediaPlayer.start();
+
+                    // When media track is finished call releaseMedia method
+                    mediaPlayer.setOnCompletionListener(onCompletionListener);
+                }
+            }
+        });
+
+        return rootView;
+    }
+
+    private void releaseMedia(){
+        if(mediaPlayer!=null){
+
+            // Release media player object
+            mediaPlayer.release();
+            mediaPlayer=null;
+
+
+            // Media player is complete, abandon audio focus
+            audioManager.abandonAudioFocus(afChangeListener);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        releaseMedia();
     }
 }
